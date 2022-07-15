d23104cdda73bbfc0c9c3b14f5d90ef691b02c79
Amit Bar-Gil <amit217@yandex.com>
2022-07-15 13:35:56 +0300

    Non duplicated imports

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
 import PopupWithForm from "./PopupWithForm";
 import ImagePopup from "./ImagePopup";
-import { useState } from "react";
 
 function App() {
   const [isEditProfilePopupOpen, setEditProfilePopupOpen] = useState(false);
