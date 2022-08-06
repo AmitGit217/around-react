cbde8520e45d8500500c3b68b925e96758c08a13
Amit Bar-Gil <amit217@yandex.com>
2022-08-06 20:19:36 +0300

    Updates

---
+++ precache-manifest.be64e8a8a544a944b6700ab726487a36.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6d25c75d6aaa0472840",
+    "url": "/around-react/static/css/main.2aed380c.chunk.css"
+  },
+  {
+    "revision": "c6d25c75d6aaa0472840",
+    "url": "/around-react/static/js/main.c6d25c75.chunk.js"
+  },
+  {
+    "revision": "d6ad0681366502a3b3ea",
+    "url": "/around-react/static/js/1.d6ad0681.chunk.js"
+  },
+  {
+    "revision": "7e810b02c3a8759631bc",
+    "url": "/around-react/static/js/2.7e810b02.chunk.js"
+  },
+  {
+    "revision": "693931bd1eb1136cb150",
+    "url": "/around-react/static/js/runtime~main.693931bd.js"
+  },
+  {
+    "revision": "2f623b332ebd64f9ffa5055aca21bdc3",
+    "url": "/around-react/static/media/Vector.2f623b33.svg"
+  },
+  {
+    "revision": "58afbe73d6547d1730a443f381c68d9e",
+    "url": "/around-react/static/media/editButton.58afbe73.svg"
+  },
+  {
+    "revision": "94185013a6a56d2102a4e27feab0b94d",
+    "url": "/around-react/static/media/likeButton.94185013.svg"
+  },
+  {
+    "revision": "3ac83020fe53b617b79b5e2ad66764af",
+    "url": "/around-react/static/media/Inter-Regular.3ac83020.woff"
+  },
+  {
+    "revision": "bdfb8ebb113b9312e080c2f4b4213223",
+    "url": "/around-react/static/media/addButton.bdfb8ebb.svg"
+  },
+  {
+    "revision": "a5ea67e3f33545f454434106b75fb6d2",
+    "url": "/around-react/static/media/likeButton_active.a5ea67e3.svg"
+  },
+  {
+    "revision": "75db5319e7e87c587019a5df08d7272c",
+    "url": "/around-react/static/media/Inter-Medium.75db5319.woff2"
+  },
+  {
+    "revision": "dc131113894217b5031000575d9de002",
+    "url": "/around-react/static/media/Inter-Regular.dc131113.woff2"
+  },
+  {
+    "revision": "c0638bea87a05fdfa2bb3bba2efe54e4",
+    "url": "/around-react/static/media/Inter-Medium.c0638bea.woff"
+  },
+  {
+    "revision": "d0b121f3a9d3d88afdfd6902d31ee9a0",
+    "url": "/around-react/static/media/Inter-Black.d0b121f3.woff"
+  },
+  {
+    "revision": "661569afe57a38e1529a775a465da20b",
+    "url": "/around-react/static/media/Inter-Black.661569af.woff2"
+  },
+  {
+    "revision": "7f080e1c934ccc5c71ef02641b0c0623",
+    "url": "/around-react/static/media/deleteButton.7f080e1c.svg"
+  },
+  {
+    "revision": "58430b3532ccf0b58af98981a0fb6d48",
+    "url": "/around-react/static/media/closeIcon.58430b35.svg"
+  },
+  {
+    "revision": "31546ed66d256411b80ce2ee40d7d48c",
+    "url": "/around-react/index.html"
+  }
+];
