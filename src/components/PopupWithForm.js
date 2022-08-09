2684b02f8573449bd4662cb40cd7a75e6b2c2807
Amit Bar-Gil <amit217@yandex.com>
2022-08-09 13:15:29 +0300

    Remove card popup style

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -2,30 +2,32 @@ import "../blocks/popup.css";
 import React from "react";
 
 export default function PopupWithForm({
-  isOpen,
-  onClose,
-  submitText,
-  title,
-  name,
-  children,
-  onSubmit,
+    isOpen,
+    onClose,
+    submitText,
+    title,
+    name,
+    children,
+    onSubmit,
 }) {
-  return (
-    <div className={`popup popup_type_${name} ${isOpen ? "popup_show" : ""}`}>
-      <div className="popup__wrapper">
-        <button
-          className="popup__close-button"
-          type="button"
-          onClick={onClose}
-        ></button>
-        <form className="popup__form" onSubmit={onSubmit}>
-          <h2 className="popup__title">{title}</h2>
-          {children}
-          <button className="popup__submit" type="submit">
-            {submitText}
-          </button>
-        </form>
-      </div>
-    </div>
-  );
+    return (
+        <div
+            className={`popup popup_type_${name} ${
+                isOpen ? "popup_show" : ""
+            }`}>
+            <div className={`popup__wrapper popup__wrapper_type_${name}`}>
+                <button
+                    className='popup__close-button'
+                    type='button'
+                    onClick={onClose}></button>
+                <form className='popup__form' onSubmit={onSubmit}>
+                    <h2 className='popup__title'>{title}</h2>
+                    {children}
+                    <button className='popup__submit' type='submit'>
+                        {submitText}
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
 }
